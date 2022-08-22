118ce112406c39bf2df0379e94e58d04ba7a0b88
MarekD97 <mdorosz2@wp.pl>
2022-08-22 14:48:22 +0200

    Add createEntry function to api_service

--- app/lib/services/api_service.dart
+++ app/lib/services/api_service.dart
@@ -6,7 +6,7 @@ import 'package:app/models/user_model.dart';
 import 'package:app/services/account_service.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<Entry>> fetchEntries([int currentPage = 1]) async {
+Future<List<Entry>> getEntries([int currentPage = 1]) async {
   User? user = await AccountService.loadUserData();
   if (user == null) {
     throw Exception('Failed to load entries: User is null');
@@ -30,6 +30,28 @@ Future<List<Entry>> fetchEntries([int currentPage = 1]) async {
     return entries;
   } else {
     throw Exception('Failed to load entries');
+  }
+}
+
+Future<Entry> createEntry(String content) async {
+  User? user = await AccountService.loadUserData();
+  if (user == null) {
+    throw Exception('Failed to create entry: User is null');
+  }
+  final Map<String, String> headers = {
+    "app-token": APP_TOKEN,
+    "user-id": user.userId.toString(),
+    "user-token": user.userToken,
+    "content-type": "application/json",
+    "accept": "application/json",
+  };
+
+  final response = await http.post(Uri.parse('$API_BASE_URL/api/entries/'),
+      headers: headers, body: jsonEncode(<String, String>{"content": content}));
+  if (response.statusCode == 201) {
+    return Entry.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to post entry');
   }
 }
 
